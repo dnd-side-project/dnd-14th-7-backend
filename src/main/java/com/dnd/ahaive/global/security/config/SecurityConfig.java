37ef6c352dcfea0e45439ddda7b99b7feb22aedf
june <0721hjp@naver.com>
2026-01-28 19:52:32 +0900

    init: SecurityConfig 추가

--- src/main/java/com/dnd/ahaive/global/security/config/SecurityConfig.java
+++ src/main/java/com/dnd/ahaive/global/security/config/SecurityConfig.java
@@ -1,5 +1,74 @@
 package com.dnd.ahaive.global.security.config;
 
+import com.dnd.ahaive.global.common.response.ResponseDTO;
+import com.dnd.ahaive.global.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  /**
+   * 예외처리 응답을 위해 사용되는 객체입니다.
+   */
+  private final ObjectMapper objectMapper;
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    // 인증 실패 시 반환할 JSON 응답
+    String invalidAuthenticationResponse = objectMapper
+        .writeValueAsString(ResponseDTO.of(ErrorCode.ACCESS_DENIED));
+
+    // 인가 실패 시 반환할 JSON 응답
+    String invalidAuthorizationResponse = objectMapper
+        .writeValueAsString(ResponseDTO.of(ErrorCode.ACCESS_DENIED));
+
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/api/**",
+                "/oauth2/**",
+                "/login/oauth2/code/**",
+                "/api/auth/**"
+            ).permitAll()
+            .anyRequest().authenticated()
+        )
+        .exceptionHandling(e -> e
+            //인증 실패 시 응답 핸들링
+            .authenticationEntryPoint(((request, response, authException) -> {
+              response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+              response.setContentType("application/json");
+              response.getWriter().write(invalidAuthenticationResponse);
+            }
+            ))
+            //인가 실패 시 응답 핸들링
+            .accessDeniedHandler((request, response, authException) -> {
+              response.setStatus(HttpStatus.UNAUTHORIZED.value());
+              response.setContentType("application/json");
+              response.getWriter().write(invalidAuthorizationResponse);
+            }));
+        return http.build();
+  }
 
 }
