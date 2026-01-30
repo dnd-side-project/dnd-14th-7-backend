6ef440249e9a6c1cf96a49aa40343ac38e76987f
june <0721hjp@naver.com>
2026-01-31 01:15:49 +0900

    feat: 텍스트 벡터 임베딩 수행 메서드 추가

---
+++ src/main/java/com/dnd/ahaive/global/exception/VectorEmbeddingException.java
@@ -0,0 +1,8 @@
+package com.dnd.ahaive.global.exception;
+
+public class VectorEmbeddingException extends CustomException {
+
+  public VectorEmbeddingException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+}
