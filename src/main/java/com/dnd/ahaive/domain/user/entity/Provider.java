203a360bd2cef3f38fae7d0c0e27004ebd8d8903
june <0721hjp@naver.com>
2026-02-10 20:05:33 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/com/dnd/ahaive/domain/user/entity/Provider.java
@@ -0,0 +1,5 @@
+package com.dnd.ahaive.domain.user.entity;
+
+public enum Provider {
+  GOOGLE
+}
---
+++ src/main/java/com/dnd/ahaive/domain/user/entity/User.java
@@ -0,0 +1,80 @@
+package com.dnd.ahaive.domain.user.entity;
+
+import com.dnd.ahaive.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+public class User extends BaseEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String userUuid;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  private String nickname;
+
+  private int credit;
+
+  @Enumerated(EnumType.STRING)
+  private Provider provider;
+
+  private String providerId;
+
+  @Builder
+  private User(
+      String userUuid,
+      Role role,
+      String nickname,
+      int credit,
+      Provider provider,
+      String providerId) {
+    this.userUuid = userUuid;
+    this.role = role;
+    this.nickname = nickname;
+    this.credit = credit;
+    this.provider = provider;
+    this.providerId = providerId;
+  }
+
+  public static User createGuest(int credit) {
+    return User.builder()
+        .userUuid(UUID.randomUUID().toString())
+        .role(Role.GUEST)
+        .nickname("Guest")
+        .credit(credit)
+        .provider(null)
+        .providerId(null)
+        .build();
+  }
+
+  public static User createMember(String nickname, int credit, Provider provider, String providerId) {
+    return User.builder()
+        .userUuid(UUID.randomUUID().toString())
+        .role(Role.MEMBER)
+        .nickname(nickname)
+        .credit(credit)
+        .provider(provider)
+        .providerId(providerId)
+        .build();
+  }
+
+
+
+}
