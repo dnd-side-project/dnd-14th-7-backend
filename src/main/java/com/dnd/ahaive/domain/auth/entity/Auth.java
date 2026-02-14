9929584782e6fd6d27b090befc8c8a10f4cf3e83
june <0721hjp@naver.com>
2026-02-14 14:01:03 +0900

    feat: auth 엔티티 추가

---
+++ src/main/java/com/dnd/ahaive/domain/auth/entity/Auth.java
@@ -0,0 +1,45 @@
+package com.dnd.ahaive.domain.auth.entity;
+
+import com.dnd.ahaive.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    name = "auth",
+    indexes = @Index(name = "idx_user_uuid", columnList = "userUuid", unique = true)
+)
+public class Auth extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String userUuid;
+
+  private String refreshToken;
+
+  @Builder
+  private Auth(String userUuid, String refreshToken) {
+    this.userUuid = userUuid;
+    this.refreshToken = refreshToken;
+  }
+
+  public static Auth of(String userUuid, String refreshToken) {
+    return Auth.builder()
+        .userUuid(userUuid)
+        .refreshToken(refreshToken)
+        .build();
+  }
+
+}
