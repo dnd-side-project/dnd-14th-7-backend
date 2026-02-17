faaad79d31839eb5ea29537fd150e9a924be9868
june <0721hjp@naver.com>
2026-02-17 19:41:15 +0900

    feat: UserController, UserService 추가

---
+++ src/main/java/com/dnd/ahaive/domain/user/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.dnd.ahaive.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 사용자 관련 API를 담당합니다.
+ * 사용자 정보 조회, 사용자 태그 전체 조회 API가 존재합니다.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+
+}
