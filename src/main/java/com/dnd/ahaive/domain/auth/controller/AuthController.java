85a1dc85b350f5a4aa492944cc52a9db94d29f6a
june <0721hjp@naver.com>
2026-02-18 16:22:57 +0900

    feat: 토큰 발급 DTO 추가

--- src/main/java/com/dnd/ahaive/domain/auth/controller/AuthController.java
+++ src/main/java/com/dnd/ahaive/domain/auth/controller/AuthController.java
@@ -1,13 +1,23 @@
 package com.dnd.ahaive.domain.auth.controller;
 
+import com.dnd.ahaive.domain.auth.dto.response.TokenResponse;
 import com.dnd.ahaive.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
   private final AuthService authService;
+
+  @PostMapping("/refresh")
+  public ResponseEntity<TokenResponse> refreshAccessToken {
+
+  }
 
 }
---
+++ src/main/java/com/dnd/ahaive/domain/auth/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.dnd.ahaive.domain.auth.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class TokenResponse {
+
+}
