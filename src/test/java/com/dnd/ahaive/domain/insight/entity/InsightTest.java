28da3eca4ca587b5ac156ad5b689335e7663e74f
june <0721hjp@naver.com>
2026-02-18 22:27:35 +0900

    test: 단위 테스트 추가 및 환경 설정 추가 - InsightTest 단위 테스트 추가 (moveToTrash, increaseView) - H2 의존성 추가 - application-test.yml 테스트 전용 yaml 추가

--- src/test/java/com/dnd/ahaive/domain/insight/entity/InsightTest.java
+++ src/test/java/com/dnd/ahaive/domain/insight/entity/InsightTest.java
@@ -1,24 +1,58 @@
 package com.dnd.ahaive.domain.insight.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.dnd.ahaive.domain.user.entity.Provider;
+import com.dnd.ahaive.domain.user.entity.User;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InsightTest {
 
   @Test
-  @DisplayName("조회수를 증가시키면 view가 1 증가함.")
+  @DisplayName("인사이트를 휴지통으로 이동시키면 trash가 true로 변경되고 trashedAt이 현재 시간으로 설정됨.")
   void moveToTrash() {
-    //TODO: 테스트 코드 작성 필요
     // given
+    User user = User.createMember(
+        "tester",
+        100,
+        "test@gmail.com",
+        Provider.GOOGLE,
+        "tester123"
+    );
 
+    Insight insight = Insight.from("초기 생각", "인사이트 제목", user);
+    LocalDateTime beforeMove = LocalDateTime.now();
 
+    // when
+    insight.moveToTrash();
+
+    //then
+    assertThat(insight.isTrash()).isTrue();
+    assertThat(insight.getTrashedAt()).isNotNull();
+    assertThat(insight.getTrashedAt()).isAfterOrEqualTo(beforeMove);
   }
 
   @Test
-  @DisplayName("인사이트를 휴지통으로 이동시키면 trash가 true로 변경되고 trashedAt이 현재 시간으로 설정됨.")
+  @DisplayName("조회수를 증가시키면 view가 1 증가함.")
   void increaseView() {
-    //TODO: 테스트 코드 작성 필요
+    // given
+    User user = User.createMember(
+        "tester",
+        100,
+        "test@gmail.com",
+        Provider.GOOGLE,
+        "tester123"
+    );
+
+    Insight insight = Insight.from("초기 생각", "인사이트 제목", user);
+    int initView = insight.getView();
+
+    // when
+    insight.increaseView();
+
+    // then
+    assertThat(insight.getView()).isEqualTo(initView + 1);
   }
 }
