54b802c40cba21615f0982094ec1ec905fdb7ac9
june <0721hjp@naver.com>
2026-02-19 22:56:26 +0900

    feat: InsightController, InsightService 추가

---
+++ src/main/java/com/dnd/ahaive/domain/insight/controller/InsightController.java
@@ -0,0 +1,16 @@
+package com.dnd.ahaive.domain.insight.controller;
+
+import com.dnd.ahaive.domain.insight.service.InsightService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/insights")
+public class InsightController {
+
+  private final InsightService insightService;
+
+}
