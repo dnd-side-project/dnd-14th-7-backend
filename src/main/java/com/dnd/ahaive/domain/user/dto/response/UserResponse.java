2aba681dbe9009777b399f2e9f44aa0e3dbbc92c
june <0721hjp@naver.com>
2026-02-19 22:49:25 +0900

    feat: 사용자 정보 조회 api 응답 필드에 position 추가

--- src/main/java/com/dnd/ahaive/domain/user/dto/response/UserResponse.java
+++ src/main/java/com/dnd/ahaive/domain/user/dto/response/UserResponse.java
@@ -1,5 +1,6 @@
 package com.dnd.ahaive.domain.user.dto.response;
 
+import com.dnd.ahaive.domain.user.entity.Position;
 import com.dnd.ahaive.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +14,14 @@ public class UserResponse {
   private String nickname;
   private String email;
   private int credit;
+  private Position position;
 
   public static UserResponse from(User user) {
     return UserResponse.builder()
         .nickname(user.getNickname())
         .email(user.getEmail())
         .credit(user.getCredit())
+        .position(user.getPosition())
         .build();
   }
 
