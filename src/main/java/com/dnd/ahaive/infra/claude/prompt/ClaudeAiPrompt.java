28573aeecef4f84c958ffc988def10f94643e2b7
june <0721hjp@naver.com>
2026-02-19 23:15:16 +0900

    feat: ai 호출을 위한 ClaudeAiClient 추가

---
+++ src/main/java/com/dnd/ahaive/infra/claude/prompt/ClaudeAiPrompt.java
@@ -0,0 +1,14 @@
+package com.dnd.ahaive.infra.claude.prompt;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class ClaudeAiPrompt {
+
+  public static String ABOUT_ME_PROMPT() {
+
+  }
+
+
+}
