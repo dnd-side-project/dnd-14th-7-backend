35ba799b4bd39f353bb0de96046c50f8b52981c6
june <0721hjp@naver.com>
2026-02-19 23:24:07 +0900

    feat: 메모 -> 제목 변환 프롬프트 추가

--- src/main/java/com/dnd/ahaive/domain/insight/service/InsightService.java
+++ src/main/java/com/dnd/ahaive/domain/insight/service/InsightService.java
@@ -2,6 +2,7 @@ package com.dnd.ahaive.domain.insight.service;
 
 import com.dnd.ahaive.domain.insight.dto.request.InsightCreateRequest;
 import com.dnd.ahaive.domain.insight.dto.response.InsightCreateResponse;
+import com.dnd.ahaive.domain.insight.entity.Insight;
 import com.dnd.ahaive.domain.user.entity.User;
 import com.dnd.ahaive.domain.user.repository.UserRepository;
 import com.dnd.ahaive.global.exception.ErrorCode;
@@ -21,6 +22,8 @@ public class InsightService {
     User user = userRepository.findByUserUuid(uuid).orElseThrow(
         () -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND)
     );
+
+    Insight insight = Insight.
 
 
 
--- src/main/java/com/dnd/ahaive/infra/claude/prompt/ClaudeAiPrompt.java
+++ src/main/java/com/dnd/ahaive/infra/claude/prompt/ClaudeAiPrompt.java
@@ -17,5 +17,17 @@ public class ClaudeAiPrompt {
         """.formatted(memo);
   }
 
+  public static String MEMO_TO_TITLE_PROMPT(String memo) {
+    return """
+        다음 메모를 핵심을 담은 짧은 제목으로 변환해줘. 제목만 출력하고 부가 설명은 절대 포함하지 마.
+        
+        예시:
+        메모: 로그 안남겨서 디버깅이 너무 힘들었다
+        출력: 서버 로그의 중요성
+        
+        메모: %s
+        """.formatted(memo);
+  }
+
 
 }
