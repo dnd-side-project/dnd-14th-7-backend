b5b0c76a23ca3e745c5aeb39717ffff53262bdaa
june <0721hjp@naver.com>
2026-02-20 09:43:14 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/dnd/ahaive/domain/user/controller/UserController.java
+++ src/main/java/com/dnd/ahaive/domain/user/controller/UserController.java
@@ -2,20 +2,16 @@ package com.dnd.ahaive.domain.user.controller;
 
 import com.dnd.ahaive.domain.user.dto.request.RegisterPositionRequest;
 import com.dnd.ahaive.domain.user.dto.response.UserResponse;
-import com.dnd.ahaive.domain.user.entity.Position;
 import com.dnd.ahaive.domain.user.service.UserService;
 import com.dnd.ahaive.global.common.response.ResponseDTO;
 import com.dnd.ahaive.global.security.core.CustomUserDetails;
 import jakarta.validation.Valid;
-import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
