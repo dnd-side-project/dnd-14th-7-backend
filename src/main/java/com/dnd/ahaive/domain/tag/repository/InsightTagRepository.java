395a73c05387820458d628d1be265dd772e6ecee
june <0721hjp@naver.com>
2026-02-21 19:52:14 +0900

    feat: 인사이트 조각 리스트 조회 api 구현

--- src/main/java/com/dnd/ahaive/domain/tag/repository/InsightTagRepository.java
+++ src/main/java/com/dnd/ahaive/domain/tag/repository/InsightTagRepository.java
@@ -1,8 +1,11 @@
 package com.dnd.ahaive.domain.tag.repository;
 
 import com.dnd.ahaive.domain.tag.entity.InsightTag;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InsightTagRepository extends JpaRepository<InsightTag, Long> {
 
@@ -12,4 +15,6 @@ public interface InsightTagRepository extends JpaRepository<InsightTag, Long> {
 
     Optional<InsightTag> findByInsightIdAndTagEntityId(Long insightId, Long tagId);
 
+    @Query("select it from InsightTag it join fetch it.tagEntity where it.insight.id = :insightId")
+    List<InsightTag> findAllByInsightId(@Param("insightId") Long insightId);
 }
