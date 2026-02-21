489a1de86f45a81d7e32668c7a1745d5b5e31d51
june <0721hjp@naver.com>
2026-02-21 11:34:01 +0900

    feat: 인사이트 상세 조회 api 응답 객체 생성

---
+++ src/main/java/com/dnd/ahaive/domain/insight/dto/response/InsightDetailResponse.java
@@ -0,0 +1,47 @@
+package com.dnd.ahaive.domain.insight.dto.response;
+
+import com.dnd.ahaive.domain.insight.entity.Insight;
+import com.dnd.ahaive.domain.tag.entity.Tag;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InsightDetailResponse {
+
+  private Long insightId;
+  private String initialThought;
+  private String title;
+
+  private List<TagResponse> tags;
+
+  private LocalDateTime createdDate;
+  private LocalDateTime updatedDate;
+
+  public static InsightDetailResponse of(Insight insight, List<Tag> tags) {
+    return InsightDetailResponse.builder()
+        .insightId(insight.getId())
+        .initialThought(insight.getInitThought())
+        .title(insight.getTitle())
+        .tags(tags.stream()
+            .map(tag -> TagResponse.builder()
+                .tagId(tag.getId())
+                .tagName(tag.getTagName())
+                .build())
+            .toList())
+        .createdDate(insight.getCreatedAt())
+        .updatedDate(insight.getUpdatedAt())
+        .build();
+  }
+
+
+  @Getter
+  @Builder
+  public static class TagResponse {
+    private Long tagId;
+    private String tagName;
+  }
+
+}
