2ad3e5612b8f182a422e93cc8dd1a653fc176efd
june <0721hjp@naver.com>
2026-02-22 21:58:33 +0900

    feat: 인사이트 후보군 재생성 api 응답 객체 추가

--- src/main/java/com/dnd/ahaive/domain/insight/dto/response/InsightCandidateReGenResponse.java
+++ src/main/java/com/dnd/ahaive/domain/insight/dto/response/InsightCandidateReGenResponse.java
@@ -1,10 +1,38 @@
 package com.dnd.ahaive.domain.insight.dto.response;
-
+import com.dnd.ahaive.domain.insight.entity.Insight;
+import com.dnd.ahaive.domain.insight.entity.InsightCandidate;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class InsightCandidateReGenResponse {
+
+  private Long insightId;
+  private Long newVersion;
+  private List<CandidateResponse> candidates;
+
+  @Getter
+  @Builder
+  public static class CandidateResponse {
+    private Long candidateId;
+    private String content;
+    private Long version;
+  }
+
+  public static InsightCandidateReGenResponse of(Insight insight, List<InsightCandidate> candidates) {
+    return InsightCandidateReGenResponse.builder()
+        .insightId(insight.getId())
+        .newVersion(candidates.get(0).getVersion())
+        .candidates(
+            candidates.stream()
+                .map(candidate -> CandidateResponse.builder()
+                    .candidateId(candidate.getId())
+                    .content(candidate.getContent())
+                    .version(candidate.getVersion()).build())
+                .toList()
+        ).build();
+  }
 
 }
