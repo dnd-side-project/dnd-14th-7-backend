e6338056bdf9a168d6251c3de5856114d32bbca0
june <0721hjp@naver.com>
2026-02-22 18:09:27 +0900

    feat: 태그별 인사이트 조회, 인사이트 전체 조회 쿼리 추가

--- src/main/java/com/dnd/ahaive/domain/insight/repository/InsightRepository.java
+++ src/main/java/com/dnd/ahaive/domain/insight/repository/InsightRepository.java
@@ -4,6 +4,8 @@ import com.dnd.ahaive.domain.insight.entity.Insight;
 import com.dnd.ahaive.domain.user.entity.User;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +17,20 @@ public interface InsightRepository extends JpaRepository<Insight, Long> {
 
     List<Insight> findByUserAndTrash(User user, boolean trash);
 
+    // 태그별 인사이트 전체 조회
+    @Query("select distinct i from Insight i "
+        + "join fetch i.insightPieces "
+        + "join fetch i.insightTags it "
+        + "join fetch it.tagEntity "
+        + "where i.user.id = :userId and i.trash = false "
+        + "and it.tagEntity.id = :tagId")
+    List<Insight> findAllByUserIdAndTagIdWithPiecesAndTags(@Param("userId") long userId, @Param("tagId") long tagId, Pageable pageable);
+
+    // 인사이트 전체 조회
+    @Query("select distinct i from Insight i "
+        + "join fetch i.insightPieces "
+        + "join fetch i.insightTags it "
+        + "join fetch it.tagEntity "
+        + "where i.user.id = :userId and i.trash = false")
+    List<Insight> findAllByUserIdWithPiecesAndTags(@Param("userId") long userId, Pageable pageable);
 }
