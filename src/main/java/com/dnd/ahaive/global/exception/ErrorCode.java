ebf11feac95ff3836e75af9753839e0e64043886
june <0721hjp@naver.com>
2026-02-24 18:32:34 +0900

    feat: 인사이트 조각을 찾을 수 없는 경우 커스텀 예외 추가

--- src/main/java/com/dnd/ahaive/global/exception/ErrorCode.java
+++ src/main/java/com/dnd/ahaive/global/exception/ErrorCode.java
@@ -93,6 +93,7 @@ public enum ErrorCode {
     INSIGHT_NOT_FOUND(404, "INSIGHT_NOT_FOUND", "해당 인사이트를 찾을 수 없습니다."),
     ANSWER_NOT_FOUND(404, "ANSWER_NOT_FOUND", "해당 답변을 찾을 수 없습니다."),
     TAG_NOT_FOUND(404, "TAG_NOT_FOUND", "해당 태그를 찾을 수 없습니다."),
+    PIECE_NOT_FOUND(404, "PIECE_NOT_FOUND", "해당 인사이트 조각을 찾을 수 없습니다."),
 
 
     // 405 Method Not Allowed
